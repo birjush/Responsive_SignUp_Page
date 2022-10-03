10f8be44a5e6eec314a8075a9d1f54282136d278
BIRJU <BIRJU@DESKTOP-K8UV98I>
2022-10-03 15:51:49 +0530

    Add project files.

---
+++ ins_fetch_entity/Controllers/employeeController.cs
@@ -0,0 +1,52 @@
+﻿using ins_fetch_entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ins_fetch_entity.Controllers
+{
+    public class employeeController : Controller
+    {
+        DBEmployeeEntities context = new DBEmployeeEntities();
+
+        [HttpGet]
+        public ActionResult GetAllEmployees()
+        {
+            List<TBLEmployee> employees = context.TBLEmployees.ToList();
+            return View("ShowEmp", employees);
+        }
+
+        public ActionResult InsertPage()
+            {
+                return View("insert");
+            }
+
+        [HttpPost]
+        public ActionResult InsertData(TBLEmployee e)
+        {
+            context.TBLEmployees.Add(e);
+            context.SaveChanges();
+            return RedirectToAction("GetAllEmployees");
+        }
+
+        public ActionResult deleterecord(string delete)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\BIRJU\source\repos\ins_fetch_entity\ins_fetch_entity\App_Data\DBEmployee.mdf;Integrated Security=True;MultipleActiveResultSets=True;Connect Timeout=30;Application Name=EntityFramework";
+            SqlCommand command = new SqlCommand();
+            string d = delete;
+            command.CommandText = $"DELETE FROM TBLEmployee WHERE name='{d}'";
+            command.Connection = con;
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+            
+
+            return RedirectToAction("GetALLEmployees");
+        }
+    }
+}
---
+++ ins_fetch_entity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ins_fetch_entity.MvcApplication" Language="C#" %>
